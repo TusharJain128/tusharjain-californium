15d880ace7a5de97290fd05a0e5455a9743c5606
TusharJain128 <tusharjain128@gmail.com>
2022-12-11 21:54:54 +0530

    Assignment of get api

---
+++ src/films.js
@@ -0,0 +1,15 @@
+const films= [ {
+    'id': 1,
+    'name': 'The Shining'
+   }, {
+    'id': 2,
+    'name': 'Incendies'
+   }, {
+    'id': 3,
+    'name': 'Rang de Basanti'
+   }, {
+    'id': 4,
+    'name': 'Finding Nemo'
+   }]
+
+   module.exports.films= films
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,27 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const intro = require('./introduction')
-const employee = require('./employee')
-const _ = require('underscore')
+const problem4 = require('../films')
 
-router.get('/test-me', function (req, res) {
-    console.log("email from introduction module", intro.myEmail)
-    intro.myFunction('Sabiha')
-    console.log("email from employee module", employee.myEmail)
-
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    let result = _.first(days, 4)
-    console.log(`Result from underscore function is ${result}`)
-
-
-    res.send('any dummy text')
+const movies=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+router.get('/movies', function (req, res) {
+    res.send(movies)
 });
 
+router.get('/movies/:indexNumber', function (req, res){
+    let requestParams = req.params
+    console.log("This is the request "+ JSON.stringify(requestParams))
+    let index = requestParams.indexNumber
+    if(index>=movies.length){
+        res.send('Please use valid index')
+    }
+    res.send(movies[index])
 
-router.get('/test-you', function(req, res){
-    console.log("I am here")
-    res.send("very important text")
+});
+
+router.get('/films', function(req, res){
+    res.send(problem4.films)
 })
 
+router.get('/films/:filmId', function( req, res){
+    let requestParams= req.params
+    let id=requestParams.filmId
+    if(films.length<id){
+        res.send('No movie exists with this id')
+    }
+    else{
+        for(let i=0; i<films.length ; i++){
+            if(films[i].id==id){
+            res.send(films[i])
+}}}})
 
 module.exports = router;
