3bf6e7cae7a3a9bf6092a108f5c07687ac61a61b
TusharJain128 <tusharjain128@gmail.com>
2022-12-12 21:42:16 +0530

    assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,38 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const problem4 = require('../films')
 
-const movies=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
-router.get('/movies', function (req, res) {
-    res.send(movies)
+router.get('/sol1', function(req, res){
+    const arr= [1,2,3,5,6,7]
+    let n= arr[arr.length-1]
+    let totalSum= n*(n+1)/2
+    let arrSum=0
+    for(let i=0; i<arr.length; i++){
+        arrSum+=arr[i]
+    }
+    const missingNumber=totalSum-arrSum
+    res.send({data:missingNumber})
 });
 
-router.get('/movies/:indexNumber', function (req, res){
-    let requestParams = req.params
-    // console.log("This is the request "+ JSON.stringify(requestParams))
-    let index = requestParams.indexNumber
-    if(index>=movies.length){
-        res.send('Please use valid index')
+router.get('/sol2', function(req,res){
+    const arr= [33, 34, 35, 37, 38]
+    let n= arr.length+1
+    let totalSum= n*(arr[0]+arr[arr.length-1])/2
+    let arrSum= 0
+    for(let i=0; i<arr.length; i++){
+        arrSum += arr[i]
     }
-    res.send(movies[index])
-
+    const missingNumber= totalSum- arrSum
+    res.send({data: missingNumber})
 });
 
-router.get('/films', function(req, res){
-    res.send(problem4.films)
-})
-
-router.get('/films/:filmId', function( req, res){
-    let requestParams= req.params
-    let id=requestParams.filmId
-    let problem5 = problem4.films
-    if(problem5.length<id){
-        res.send('No movie exists with this id')
-    }
-    else{
-        for(let i=0; i<problem5.length ; i++){
-            if(problem5[i].id==id){
-            res.send(problem5[i])
-}}}})
 
 module.exports = router;
