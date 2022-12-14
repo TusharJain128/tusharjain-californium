08b68415e78df200126eca76fdf036d7beb6dbbe
TusharJain128 <tusharjain128@gmail.com>
2022-12-14 16:41:25 +0530

    query param assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,46 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
-let players =
-   [
-       {
-           "name": "manish",
-           "dob": "1/1/1995",
-           "gender": "male",
-           "city": "jalandhar",
-           "sports": [
-               "swimming"
-           ]
-       },
-       {
-           "name": "gopal",
-           "dob": "1/09/1995",
-           "gender": "male",
-           "city": "delhi",
-           "sports": [
-               "soccer"
-           ]
-       },
-       {
-           "name": "lokesh",
-           "dob": "1/1/1990",
-           "gender": "male",
-           "city": "mumbai",
-           "sports": [
-               "soccer"
-           ]
-       }
-   ]
-router.post('/players', function(req, res){
-    let obj= req.body
-    for(let i=0; i<players.length; i++){
-         if(players[i].name=== obj.name){
-           res.send('Name is already exist')
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+
+ router.post('/person', function(req,res){
+    let voteAge= req.query.votingAge
+    for(let i=0; i<persons.length; i++){
+        if(persons[i].age>=voteAge){
+            persons[i].votingStatus= true
         }
     }
-    players.push(obj)
-    res.send({data: players, status:true})
-})
+    let result= persons.filter(ele=> ele.votingStatus)
+    res.send({data: result})
+ })
 
 
 module.exports = router;
