7216774774adac09b5d451d8f996e0499f72976a
TusharJain128 <tusharjain128@gmail.com>
2022-12-26 22:09:20 +0530

    solution of middleware 2

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb+srv://TusharJainFunctionup:functionup@tusharjaindb.zxey2fj.mongodb.net/test", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,9 +4,6 @@ const userController= require("../controllers/userController")
 const productController = require("../controllers/productController")
 const orderController = require("../controllers/orderController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
 router.post("/products", productController.createProduct)
 router.post("/users", userController.createUser)
