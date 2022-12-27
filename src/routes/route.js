a2d3182850b17472c85fafaf7f5cb166593970dc
TusharJain128 <tusharjain128@gmail.com>
2022-12-27 20:11:20 +0530

    auth1

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,17 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-router.post("/users", userController.createUser  )
-
-router.post("/login", userController.loginUser)
-
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-
-router.put("/users/:userId", userController.updateUser)
+router.post('/createUser',userController.registerUser)
+router.post('/loginUser',userController.loginUser)
+router.get('/userDetails/:userId',userController.getUserDetails)
+router.put('/updateUser/:userId',userController.updateUser)
+router.delete('/deleteUser/:userId',userController.deleteUser)
 
 module.exports = router;
